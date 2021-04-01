755fb89e71b0590d2a7c0a185ff20db3a746dc2d
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-01 11:06:14 +0300

    [Feat] input(select)-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import './App.scss';
-import InputSelect from './Components/InputSelect';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to the project</h1>
-      <InputSelect />
     </div>
   );
 }
--- client/src/Components/InputSelect/index.js
+++ client/src/Components/InputSelect/index.js
@@ -5,6 +5,7 @@ import './InputSelect.scss';
 const InputSelect = ({ options, handleInputChange }) => {
   return (
     <select onChange={handleInputChange} className="select" placeholder="Choose">
+      <option value="">Select an Option</option>
       {options.map((opt, index) => (
         <option value={opt} key={index}>
           {opt}
