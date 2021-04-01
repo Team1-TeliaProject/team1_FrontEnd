b8a3f3c414a4f60e1b5b4eab9a3ce4a1f3264446
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2021-04-01 12:51:58 +0300

    [Feat/Footer Component] - restructured footer component entirely and ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
-import './scss/app.scss';
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import About from './Components/footer/About';
-import Home from './Components/Home';
-import Privacy from './Components/footer/Privacy';
-
+import Footer from './Components/Footer'; 
 
 
 function App() {
   return (
     <div className="app">
-      <div className="app__main">
-      <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/about">
-                        <About />
-                    </Route>
+      <h1>Welcome to the project</h1>
                 
-                
-                    <Route path="/privacy">
-                        <Privacy />
-                    </Route>
-                </Switch>
-                
-            </Router>
-      </div>
-      
-   
+          <Footer />
     </div>
   );
 }
