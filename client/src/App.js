64d8afd2e114c72cf02a3df0b78326aaf0acc602
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-01 15:16:53 +0300

    [Feat] navigation-component ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import './App.scss';
-import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <div className="app">
-      <Navigation />
-      {/* <h1>Welcome to the project</h1> */}
+      <h1>Welcome to the project</h1>
     </div>
   );
 }
