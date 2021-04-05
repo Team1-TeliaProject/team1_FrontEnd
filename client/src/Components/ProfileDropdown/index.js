31d070472e820deb8279477db11b8ad4f46f0b49
shresthapr <shresthaprajan38@gmail.com>
2021-04-05 19:51:14 +0300

    feat/profileDrop... onClick event added

--- client/src/Components/ProfileDropdown/index.js
+++ client/src/Components/ProfileDropdown/index.js
@@ -5,16 +5,16 @@ import { IoLogOutOutline } from 'react-icons/io5';
 
 import './profiledropdn.scss';
 
-const ProfileDropdown = () => {
+const ProfileDropdown = ({ handleEdit, handleExit }) => {
   return (
     <>
       <section className="profDropDn">
-        <div className="profDropDn__edit">
+        <div className="profDropDn__edit" onClick={handleEdit}>
           <AiTwotoneEdit style={{ height: '4em', width: '2em', paddingLeft: '1rem' }} />
           <p className="profDropDn__edit profDropDn__edit__text">Profile</p>
         </div>
 
-        <div className="profDropDn__logout">
+        <div className="profDropDn__logout" onClick={handleExit}>
           <IoLogOutOutline style={{ height: '4em', width: '2em', paddingLeft: '1rem' }} />
           <p className="profDropDn__logout profDropDn__logout__text">LogOut</p>
         </div>
