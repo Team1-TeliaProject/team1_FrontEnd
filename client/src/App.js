371b692a770847532a13bb33116ca91afe6c908c
shresthapr <shresthaprajan38@gmail.com>
2021-04-05 17:29:11 +0300

    dev/FIX --footer component removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Footer from './Components/Footer'; 
-
 
 import './App.scss';
 
@@ -8,8 +6,6 @@ function App() {
   return (
     <div className="app">
       <h1>Welcome to the project</h1>
-                
-          <Footer />
     </div>
   );
 }
