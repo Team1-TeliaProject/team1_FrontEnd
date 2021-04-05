e73d2ab8d52a9e7d40fecbb4b4a97c2ad8e4c112
shresthapr <shresthaprajan38@gmail.com>
2021-04-05 19:43:11 +0300

    [feat/profileDrop..] profile component + app.scss responsiveness modiifed

---
+++ client/src/Components/ProfileDropdown/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { AiTwotoneEdit } from 'react-icons/ai';
+import { IoLogOutOutline } from 'react-icons/io5';
+
+import './profiledropdn.scss';
+
+const ProfileDropdown = () => {
+  return (
+    <>
+      <section className="profDropDn">
+        <div className="profDropDn__edit">
+          <AiTwotoneEdit style={{ height: '4em', width: '2em', paddingLeft: '1rem' }} />
+          <p className="profDropDn__edit profDropDn__edit__text">Profile</p>
+        </div>
+
+        <div className="profDropDn__logout">
+          <IoLogOutOutline style={{ height: '4em', width: '2em', paddingLeft: '1rem' }} />
+          <p className="profDropDn__logout profDropDn__logout__text">LogOut</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ProfileDropdown;
