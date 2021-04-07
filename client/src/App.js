5cce71106243e3290df02d58cd9d292561cb32b2
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-07 13:00:44 +0300

    [Feat] Landing page done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Navigation from './Components/Navigation';
 import Footer from './Components/Footer';
-import LandingPage from './pages/LandingPage';
 
 import './App.scss';
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="app">
       <Navigation />
-      <LandingPage />
       <Footer />
     </div>
   );
