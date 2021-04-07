3d9e6f572076ed556884171cb107147cd9c376d7
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-04-07 12:05:30 +0300

    [Example] example for naming scss classnames

--- client/src/Components/Example/index.js
+++ client/src/Components/Example/index.js
@@ -4,8 +4,17 @@ import './Button.scss';
 
 const Button = ({ text, handleClick, modifier }) => {
   return (
-    <div className="main">
-      <button className={`main__button main__button--${modifier}`}>{text}</button>
+    <div>
+      <div className="main">
+        <button className={`main__button main__button--${modifier}`}>{text}</button>
+      </div>
+      {/* naming style */}
+      <div className="employee-card">
+        <div className="employee-card__section1">
+          <p className="employee-card__text"></p>
+          <p className="employee-card__text  employee-card__text--big-text"></p>
+        </div>
+      </div>
     </div>
   );
 };
