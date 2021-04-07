d3fe7eb4bb5543f7cfca347b460a2c4acc53c6c8
shresthapr <shresthaprajan38@gmail.com>
2021-04-07 12:02:35 +0300

    Revert "feat/jobCards  responsive design added"
    This reverts commit f7d1ce4f to 54e571a.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import './App.scss';
 
-import Jobcard from './Components/Jobscard/index';
 function App() {
   return (
     <div className="app">
       <h1>Welcome to the project</h1>
-      <Jobcard
-        user="applicant"
-        jobtitle="Head of department"
-        company="Business College Helsinki"
-        deadline="04.04.2021"
-        location="Helsinki Finland"
-        type="Full-time"
-        level="junior"
-        skills="React ,Nodejs, TypeScript, AWS, Python, and everything that has to be known"
-        applicantName="Mr Smith"
-        applicantTitle="Student"
-        applicantLevel="Mid-Senior"
-        applicantType="part-time, Full-time"
-        applicantSkills="Nothing interesting or worth mentioning"
-        applicantLocation="Davos Switzerland"
-        availability="03.04.2030"
-      />
     </div>
   );
 }
