d7e4e80c541ac4f09ea20763257a488026581231
shresthapr <shresthaprajan38@gmail.com>
2021-04-07 16:34:17 +0300

    feat/profileDrop... closing dropdown on clicking outside added

--- client/src/Components/ProfileDropdown/index.js
+++ client/src/Components/ProfileDropdown/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import EscapeOutside from 'react-escape-outside'
 import { AiTwotoneEdit } from 'react-icons/ai';
 import { IoLogOutOutline } from 'react-icons/io5';
 
 import './profiledropdn.scss';
 
 const ProfileDropdown = ({ handleEdit, handleExit }) => {
+  const [isOpen, setIsOpen]= useState(true);
+  const handleEscapeOutside=()=> {
+    setIsOpen(false )
+  }
+
   return (
     <>
-      <section className="dropdown">
+         <EscapeOutside onEscapeOutside={ handleEscapeOutside }>
+      {isOpen ? (<section className="dropdown" >
         <div className="dropdown__edit" onClick={handleEdit}>
           <AiTwotoneEdit className="dropdown__icon" />
           <p className="dropdown__text">Profile</p>
@@ -17,8 +23,11 @@ const ProfileDropdown = ({ handleEdit, handleExit }) => {
         <div className="dropdown__logout" onClick={handleExit}>
           <IoLogOutOutline className="dropdown__icon" />
           <p className="dropdown__text">Log Out</p>
+       
         </div>
-      </section>
+      </section>) : <> </>}
+      </EscapeOutside>
+
     </>
   );
 };
