a7e2e09f71e7bf3db84793096a3dfd03419eff48
Sahar Perveen <s1900535@edu.bc.fi>
2021-04-07 22:27:58 +0300

    feat/match-for-employee/ reformated css and index.js

--- client/src/Components/MatchedJob/index.js
+++ client/src/Components/MatchedJob/index.js
@@ -6,21 +6,21 @@ import InfoButton from '../InfoButton';
 import {Container, Row, Col} from 'react-bootstrap';
 
 
-const MatchedJob = ({ jobTitle, companyName, positionLevel, location, squareColor, chatClicked, infoClicked }) => {
+const MatchedJob = ({ match, modifier, chatClicked, infoClicked }) => {
   return (
           <Container >
               <Row className='matched-job'>
-                  <Col className='col-box square' style={{backgroundColor:{squareColor}}}></Col>
+                  <Col className='col-box square {modified}'></Col>
                   <Col xs={5} className='col-content'>
                       <Container className='inner-container'>
-                          <Row><Col><p className='job-title'>{jobTitle}</p></Col></Row>
-                          <Row><Col><p className='company-name'>{companyName}</p></Col></Row>
+                          <Row><Col><p className='job-title'>{match.jobTitle}</p></Col></Row>
+                          <Row><Col><p className='company-name'>{match.companyName}</p></Col></Row>
                       </Container>
                   </Col>
                   <Col xs={2} className='col-content'>
                       <Container className='inner-container'>
-                          <Row><Col><p className='job-position-level'>{positionLevel}</p></Col></Row>
-                          <Row><Col><p className='location'>{location}</p></Col></Row>
+                          <Row><Col><p className='job-position-level'>{match.positionLevel}</p></Col></Row>
+                          <Row><Col><p className='location'>{match.location}</p></Col></Row>
                       </Container>
                   </Col>
                   <Col xs={2} className='col-content last-col'>
