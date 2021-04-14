658ca0ffcb36cc8e77711f071b3aacd964356bf9
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2021-04-14 12:46:37 +0300

    [feat/company-profile] - restructure style

--- client/src/pages/TryOutPage/index.js
+++ client/src/pages/TryOutPage/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import CompanyProfile from '../../Components/CompanyProfile/index';
 
 import './TryOutPage.scss';
 
 const TryOutPage = () => {
   return (
     <div className="tryout">
-      <h1>check out your components here</h1>
-      
+      {/* <h1>check out your components here</h1> */}
+      <CompanyProfile
+        title="Frontend Developer"
+        position="Junior Developer"
+        name="Emma Stone"
+        location="Tampere"
+      />
     </div>
   );
 };
