0c1e99c523d735799661b3d142a281af11501e5e
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-05-06 10:19:05 +0300

    [Feat] heroku links

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000/api/login';
+// const baseUrl = 'http://localhost:5000/api/login';
+const baseUrl = 'https://duuni-app.herokuapp.com/api/login';
 
 export const logUser = async (credentials) => {
-  console.log('test--', credentials);
   return await axios.post(baseUrl, credentials);
 };
--- client/src/services/matchService.js
+++ client/src/services/matchService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000/api/matches';
+const baseUrl = 'https://duuni-app.herokuapp.com/api/matches';
 
 export const createMatch = async (matchInfo) => {
   return await axios.post(baseUrl, matchInfo);
