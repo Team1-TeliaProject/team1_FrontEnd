6db9e0d9a2c66d5915250bb6ad68a9ad6a6815cc
Sahar Perveen <s1900535@edu.bc.fi>
2021-05-21 08:16:31 +0300

    feat/chat-socket-io/components for chat

--- src/Components/Chat/index.js
+++ src/Components/Chat/index.js
@@ -1,45 +1,45 @@
 import React from 'react';
 
 import './Chat.scss';
-const Chat = ({ chat }) => {
+const Chat = ({ chat, user }) => {
   return (
     <div className="chat">
-      <p className="chat__time">{chat.time}</p>
-      {chat.userId === 'abc123' ? (
-        <div className="chat__message-div">
-          <img
-            className={chat.userId === 'abc123' ? 'chat__image ' : 'chat__image chat__image--in'}
-            src="https://www.foodwest.fi/wp-content/uploads/2016/11/blank-profile-picture-973460_1280.png"
-            alt="W3Schools.com"
-          />
-          <div
-            className={
-              chat.userId === 'abc123'
-                ? 'chat__message chat__message--out'
-                : 'chat__message chat__message--in'
-            }
-          >
-            {chat.message}
-          </div>
-        </div>
-      ) : (
-        <div className="chat__message-div">
-          <div
-            className={
-              chat.userId === 'abc123'
-                ? 'chat__message chat__message--out'
-                : 'chat__message chat__message--in'
-            }
-          >
-            {chat.message}
-          </div>
-          <img
-            className={chat.userId === 'abc123' ? 'chat__image ' : 'chat__image chat__image--in'}
-            src="https://www.foodwest.fi/wp-content/uploads/2016/11/blank-profile-picture-973460_1280.png"
-            alt="W3Schools.com"
-          />
-        </div>
-      )}
+        <p className="chat__time">{new Date(chat.created_at).toLocaleTimeString()}</p>
+        {chat.from === user.userId ? (
+            <div className="chat__message-div">
+                <div
+                    className={
+                        chat.from === user.userId
+                            ? 'chat__message chat__message--out'
+                            : 'chat__message chat__message--in'
+                    }
+                >
+                    {chat.message}
+                </div>
+                <img
+                    className={chat.from === user.userId ? 'chat__image ' : 'chat__image chat__image--in'}
+                    src={user.photo}
+                    alt="W3Schools.com"
+                />
+            </div>
+        ) : (
+            <div className="chat__message-div">
+                <img
+                    className={chat.from === user.userId ? 'chat__image ' : 'chat__image chat__image--in'}
+                    src={user.photo}
+                    alt="W3Schools.com"
+                />
+                <div
+                    className={
+                        chat.from === user.userId
+                            ? 'chat__message chat__message--out'
+                            : 'chat__message chat__message--in'
+                    }
+                >
+                    {chat.message}
+                </div>
+            </div>
+        )}
     </div>
   );
 };
--- src/Components/Messages/index.js
+++ src/Components/Messages/index.js
@@ -1,19 +1,71 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Chat from '../Chat';
 import Input from '../Input';
 import { IoMdSend } from 'react-icons/io';
 
 import './Messages.scss';
-const Messages = ({ chats }) => {
-  const [message, setMessage] = useState('');
-  console.log(message);
+import io from "socket.io-client";
 
-  return (
+const ENDPOINT = 'http://localhost:5000/';
+const Messages = ({ userInfo, matchedUser}) => {
+
+    //*****************************
+    //*******CHAT CODE START*******
+    //*****************************
+    const socket = io(ENDPOINT);
+    const [text, setText] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [contactRoom, setContactRoom] = useState();
+
+    const addNewMessage = (message) => {
+        setMessages(messages => [...messages, message])
+    }
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        event.target.blur()
+        const message = {
+            created_at: new Date(Date.now()).toISOString(),
+            message: text,
+            to: matchedUser.id,
+            from: userInfo.userId,
+        }
+
+        addNewMessage(message)
+        socket.emit("push new message", message, contactRoom);
+        setText('')
+    }
+
+    //Join Room
+    useEffect(() => {
+        console.log(userInfo);
+        setContactRoom(userInfo.userId + matchedUser.id)
+
+        socket.emit("join", {
+            me: userInfo.userId,
+            contact: matchedUser.id,
+            room: userInfo.userId + matchedUser.id,
+        },(error) => {
+            if(error) {
+                alert('ERROR : '+error);
+            }
+        });
+
+        socket.on("get message history", setMessages)
+
+    }, [matchedUser]);
+
+    //New Message
+    useEffect(() => {
+        socket.on("new message", (message) => addNewMessage(message))
+    }, [messages]);
+
+    return (
     <div className="messages">
       <div className="messages__chat-div">
-        {chats.map((item, index) => (
-          <Chat chat={item} key={index} />
+        {messages.map((item, index) => (
+            <Chat chat={item} user={userInfo} key={index} />
         ))}
       </div>
       <hr className="messages__hr" />
@@ -21,10 +73,13 @@ const Messages = ({ chats }) => {
         <div className="messages__btn-div">
           <Input
             placeholder="Write your message"
-            handleInputChange={(e) => setMessage(e.target.value)}
+            type='text'
+            value={text}
+            handleInputChange={({ target: { value } }) => setText(value)}
+            onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
           />
         </div>
-        <IoMdSend className="messages__icon" />
+        <IoMdSend className="messages__icon" onClick={e => sendMessage(e)}/>
       </div>
     </div>
   );
